44cfa7a18877059401c131b6c56ec04cce216b47
pruzoth <pruzoth@gmail.com>
2014-08-13 01:04:50 +0530

    Template generate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'demo#index'
+
+  match ':controller(/:action(/:id))', :via => :get
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
