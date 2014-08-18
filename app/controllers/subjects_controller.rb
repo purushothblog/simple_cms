2ad80c8069f42de8fd123020d52e4a6e5256c89c
pruzoth <pruzoth@gmail.com>
2014-08-18 14:42:58 +0530

    Add delete

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -37,6 +37,12 @@ class SubjectsController < ApplicationController
   end
 
   def delete
+    @subject = Subject.find(params[:id])
+  end
+
+  def destroy
+    subject = Subject.find(params[:id]).destroy
+    redirect_to(:action => 'index')
   end
 
   private
