790aee137554832b6f5d8180126951e9f614c467
pruzoth <pruzoth@gmail.com>
2014-08-18 17:58:11 +0530

    Add Page Controller

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -3,9 +3,11 @@ class Subject < ActiveRecord::Base
   has_many :pages
 
   scope :visible, lambda { where(:visible => true) }
-  scope :invisible, lambda {  where(:visible => false) }
+  scope :invisible, lambda { where(:visible => false) }
   scope :sorted, lambda { order("subjects.position ASC") }
-  scope :newest_first, lambda { order("subjects.created_at DESC") }
-  scope :search, lambda { |query|
-   where(["name LIKE ?", "%#{query}%"]) }
+  scope :newest_first, lambda { order("subjects.created_at DESC")}
+  scope :search, lambda {|query|
+    where(["name LIKE ?", "%#{query}%"])
+  }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
+  get 'sections/index'
+
+  get 'sections/show'
+
+  get 'sections/new'
+
+  get 'sections/edit'
+
+  get 'sections/delete'
+
+  get 'pages/index'
+
+  get 'pages/show'
+
+  get 'pages/new'
+
+  get 'pages/edit'
+
+  get 'pages/delete'
+
   root 'demo#index'
 
   match ':controller(/:action(/:id))', :via => [:get, :post]
