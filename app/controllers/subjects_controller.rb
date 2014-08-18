ea9d79a46738543357a9a7bffa353f46830e0026
pruzoth <pruzoth@gmail.com>
2014-08-18 14:19:57 +0530

    add update

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -23,6 +23,19 @@ class SubjectsController < ApplicationController
     end
   end
 
+  def edit
+    @subject = Subject.find(params[:id])
+  end
+
+  def update
+    @subject = Subject.find(params[:id])
+    if @subject.update_attributes(subject_params)
+        redirect_to(:action => 'show', :id => @subject.id)
+    else
+        render('edit')
+    end
+  end
+
   def delete
   end
 
